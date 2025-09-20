67441631871b10d5c90a4693c28c177563b7b29f
diegargon <diegargon@gmail.com>
2025-09-20 16:40:58 +0200

    Update repo 2025-09-20

--- rebuild_repo.sh
+++ rebuild_repo.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Configuration
+# Downlaod the latest kernel packages from releases
+KERNEL_VERSION="6.12.48-14-styx"
+REVISION="14"
+wget -c https://github.com/sty-firewall/styx-kernel/releases/download/v0.${REVISION}/linux-headers-${KERNEL_VERSION}_${REVISION}_amd64.deb
+wget -c https://github.com/styx-firewall/styx-kernel/releases/download/v0.${REVISION}/linux-image-${KERNEL_VERSION}_${REVISION}_amd64.deb
+wget -c https://github.com/styx-firewall/styx-kernel/releases/download/v0.${REVISION}/linux-libc-dev_${REVISION}_amd64.deb
+# TODO styx-packages
+
+# Configuration Repository
 GPG_KEY_ID="diegargon@"
 KEY_FILENAME="styx-firewall-keyring.gpg"
 REPO_BASE="."
